5e4356aa2113a4ec926b3219280f53798622445f
laizz42 <felixhellmich@protonmail.ch>
2021-01-02 22:17:53 +0000

    Created and added zcrack.py

---
+++ zcrack.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python2
+
+# import
+import zipfile, argparse
+
+# set optional arguments
+parser = argparse.ArgumentParser(description='ZIP Archive Cracker')
+parser.add_argument('-t', '--target', type=str, metavar='', required=True, help='Path to target ZIP archive')
+parser.add_argument('-w', '--wordlist', type=str, metavar='', required=True, help='Path to wordlist')
+args = parser.parse_args()
+
+# open wordlist and set line_log
+passwords = open(args.wordlist,"r").readlines()
+# use wordlist to bruteforce the password
+for password in passwords:
+    passwd = password.rstrip()
+    # use passwd, try passwd
+    try:
+        # set args.target as target and if match found print match
+        with zipfile.ZipFile(args.target) as target_zip:
+            target_zip.extractall(pwd=passwd)
+            print("[*] Password Match Found! %s" % passwd)
+            break
+    except:
+        # no match found
+        print("[I] No Match Found!")
+
+
+
+
